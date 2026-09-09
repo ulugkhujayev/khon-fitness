9f6addec2ef4d6486811d967a650b0376d3d0d18
Mirzohidkhon <ulugkhujayevmirohidxon@gmail.com>
2026-09-09 14:13:37 +0500

    0.3.3: version bump to exercise the one-tap update flow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.mirzohidkhon.khonfitness"
         minSdk = 28
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.3.2"
+        versionCode = 14
+        versionName = "0.3.3"
     }
 
     signingConfigs {
